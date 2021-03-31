b335a440afcfa831c1a30ecfe9a34866fd0470c1
Michael Burman <yak@iki.fi>
2021-03-31 12:27:20 +0300

    Update to link to k8ssandra repositories and k8ssandra docs instead of old Datastax resources
    Update manifests to 1.7.0 supported form, remove per-kubernetes-ones
    
    Remove -ver manifest links
    
    Rename back to DataStax Cass Operator

--- hack/release/make-yaml-bundle.sh
+++ hack/release/make-yaml-bundle.sh
@@ -10,7 +10,7 @@ bundle="$(mktemp)"
 k8sVer="$(kubectl version --short | grep Server | egrep -o 'v[0-9].[0-9]+')"
 
 echo '---' >> "$bundle"
-cat operator/deploy/namespace.yaml | yq r - >> "$bundle"
+cat operator/deploy/namespace.yaml | yq eval - >> "$bundle"
 
 echo '---' >> "$bundle"
 helm template ./charts/cass-operator-chart/ -n cass-operator --validate=true | kubectl create --dry-run=client -o yaml -n cass-operator -f - >> "$bundle"

84eb538d214670bb93a088b625ecaad9dc97f044
Michael Burman <michael.burman@datastax.com>
2021-05-07 16:59:24 +0300

    Merge from 1.7.0 (#31)
    * update changelog for 1.7.0
    
    * Set system-logger to newest tested version instead of latest for 1.7.0
    
    Co-authored-by: John Sanda <john.sanda@gmail.com>

--- operator/pkg/images/images.go
+++ operator/pkg/images/images.go
@@ -103,7 +103,7 @@ var imageLookupMap map[Image]string = map[Image]string{
 	UBIConfigBuilder: "datastax/cass-config-builder:1.0.3-ubi7",
 
 	BusyBox:           "busybox:1.32.0-uclibc",
-	SystemLoggerImage: "k8ssandra/system-logger:latest",
+	SystemLoggerImage: "k8ssandra/system-logger:9c4c3692",
 }
 
 var versionToOSSCassandra map[string]Image = map[string]Image{

92401211d5c388cdab378b93f403c7e7830d08e8
Miles-Garnsey <miles.garnsey@datastax.com>
2021-09-19 21:36:36 +1000

    Add logic to automatically enable/disable full query logging facility in line with the cassandra.yaml.

--- pkg/reconciliation/reconcile_racks.go
+++ pkg/reconciliation/reconcile_racks.go
@@ -4,9 +4,11 @@
 package reconciliation
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -2243,6 +2245,65 @@ func (rc *ReconciliationContext) fixMissingPVC() (bool, error) {
 	return false, nil
 }
 
+func (rc *ReconciliationContext) SetFullQueryLogging() result.ReconcileResult {
+	shouldFQLBeEnabled := false
+	dc := rc.GetDatacenter()
+	if dc.Spec.Config != nil {
+		var dcConfig map[string]interface{}
+		if err := json.Unmarshal(dc.Spec.Config, &dcConfig); err != nil {
+			rc.ReqLogger.Error(err, "error unmarshalling DC config JSON")
+			return result.RequeueSoon(2)
+		}
+		casYaml, found := dcConfig["cassandra-yaml"]
+		if !found {
+			return result.Continue()
+		}
+		casYamlMap, ok := casYaml.(map[string]interface{})
+		if !ok {
+			err := fmt.Errorf("type casting error")
+			rc.ReqLogger.Error(err, "couldn't cast cassandra-yaml value from config to map[string]interface{}")
+			return result.RequeueSoon(2)
+		}
+		if _, found := casYamlMap["full_query_logging_options"]; found {
+			serverMajorVersion, err := strconv.ParseInt(strings.Split(dc.Spec.ServerVersion, ".")[0], 10, 8)
+			if err != nil {
+				rc.ReqLogger.Error(err, "error parsing server major version. Can't enable full query logging without knowing this")
+				return result.RequeueSoon(2)
+			}
+			if serverMajorVersion < 4 {
+				err := fmt.Errorf("full query logging only supported on OSS Cassandra 4x+")
+				rc.ReqLogger.Error(err, "full_query_logging_options is defined in Cassandra config, it is not supported on the version of Cassandra you are running")
+				return result.RequeueSoon(2)
+			}
+			rc.ReqLogger.Info("full_query_logging_options is defined in Cassandra config, we will try to enable it via the management API")
+			shouldFQLBeEnabled = true
+		}
+	}
+
+	podList, err := rc.listPods(rc.Datacenter.GetClusterLabels())
+	if err != nil {
+		rc.ReqLogger.Error(err, "error listing all pods in the cluster to progress full query logging reconciliation")
+		return result.RequeueSoon(2)
+	}
+	for _, podPtr := range PodPtrsFromPodList(podList) {
+		fqlEnabledForPod, err := rc.NodeMgmtClient.CallIsFullQueryLogEnabledEndpoint(podPtr)
+		if err != nil {
+			rc.ReqLogger.Error(err, "can't get whether query logging enabled for pod ", "podName", podPtr.Name)
+			return result.RequeueSoon(2)
+		}
+		rc.ReqLogger.Info("full query logging status:", "isEnabled", fqlEnabledForPod, "shouldBeEnabled", shouldFQLBeEnabled)
+		if fqlEnabledForPod != shouldFQLBeEnabled {
+			rc.ReqLogger.Info("Setting full query logging on ", "podIP", podPtr.Status.HostIP, "podName", podPtr.Name, "fqlDesiredState", shouldFQLBeEnabled)
+			err := rc.NodeMgmtClient.CallSetFullQueryLog(podPtr, shouldFQLBeEnabled)
+			if err != nil {
+				rc.ReqLogger.Error(err, "couldn't enable full query logging on ", "podIP", podPtr.Status.HostIP, "podName", podPtr.Name)
+				return result.RequeueSoon(2)
+			}
+		}
+	}
+	return result.Continue()
+}
+
 // ReconcileAllRacks determines if a rack needs to be reconciled.
 func (rc *ReconciliationContext) ReconcileAllRacks() (reconcile.Result, error) {
 	if recResult := rc.CheckForInvalidState(); recResult.Completed() {
@@ -2354,6 +2415,10 @@ func (rc *ReconciliationContext) ReconcileAllRacks() (reconcile.Result, error) {
 	}
 
 	if recResult := rc.CheckConditionInitializedAndReady(); recResult.Completed() {
+		return recResult.Output()
+	}
+
+	if recResult := rc.SetFullQueryLogging(); recResult.Completed() {
 		return recResult.Output()
 	}
 

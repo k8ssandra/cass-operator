8cfc4c542e73c9c06c4b77403d5be020620723b4
Michael Burman <yak@iki.fi>
2022-02-16 13:46:07 +0200

    Add checks to scripts, fix some bugs

--- scripts/post-release-process.sh
+++ scripts/post-release-process.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 IMG=k8ssandra/cass-operator:latest
-LOG_IMG=k8ssandra/system-logger:latest
 
 # Add new ## unreleased after the tagging (post-release-process.sh)
 # Modify Makefile for the next VERSION in line
 
 # Return config/manager/kustomization.yaml to :latest
-cd config/manager && bin/kustomize edit set image controller=$(IMG)
+cd config/manager && $KUSTOMIZE edit set image controller=$IMG && cd -
 
 # Return config/manager/image_config.yaml to :latest
-yq eval -i '.images.system-logger = env(LOG_IMG)' config/manager/image_config.yaml
+LOG_IMG=k8ssandra/system-logger:latest yq eval -i '.images.system-logger = env(LOG_IMG)' config/manager/image_config.yaml
--- scripts/pre-release-process.sh
+++ scripts/pre-release-process.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: scripts/pre-release-process.sh newTag previousTag"
+    exit
+fi
+
 TAG=$1
 PREVTAG=$2
 #PREVTAG=$(git describe --abbrev=0 --tags)
-IMG=k8ssandra/cass-operator:$TAG
-LOG_IMG=k8ssandra/system-logger:$TAG
+IMG=k8ssandra/cass-operator:${TAG}
+
+# Ensure kustomize is installed
+make kustomize
+
+KUSTOMIZE=$(pwd)/bin/kustomize
 
 # Modify CHANGELOG automatically to match the tag
 sed -i -e "s/## unreleased/## $TAG/" CHANGELOG.md
@@ -13,7 +22,7 @@ sed -i -e "s/## unreleased/## $TAG/" CHANGELOG.md
 sed -i -e "s/$PREVTAG/$TAG/g" README.md
 
 # Modify config/manager/kustomization.yaml to have proper newTag for cass-operator
-cd config/manager && bin/kustomize edit set image controller=$(IMG)
+cd config/manager && $KUSTOMIZE edit set image controller=$IMG && cd -
 
 # Modify config/manager/image_config.yaml to have proper version for server-system-logger
-yq eval -i '.images.system-logger = env(LOG_IMG)' config/manager/image_config.yaml
+LOG_IMG=k8ssandra/system-logger:${TAG} yq eval -i '.images.system-logger = env(LOG_IMG)' config/manager/image_config.yaml

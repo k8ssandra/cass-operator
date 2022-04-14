f83ae642c011ac2e8b8dd6c05a11750f6482982b
Michael Burman <michael.burman@datastax.com>
2022-04-14 16:26:46 +0300

    Create catalog files after master merges, add phony targets to Makefile, modify CSV to include minKubeVersion and the sample. Change min. Openshift version to 4.9

--- scripts/postprocess-bundle.sh
+++ scripts/postprocess-bundle.sh
@@ -3,13 +3,13 @@
 # These labels are required to be in the bundle.Dockerfile, but can't be added by the operator-sdk automatically
 cat <<EOF >> bundle.Dockerfile
 # Certified Openshift required labels
-LABEL com.redhat.openshift.versions="v4.7"
+LABEL com.redhat.openshift.versions="v4.9"
 LABEL com.redhat.delivery.operator.bundle=true
 LABEL com.redhat.delivery.backport=true
 EOF
 
 # Add them to the bundle metadata also
-yq eval -i '.annotations."com.redhat.openshift.versions" = "v4.7"' bundle/metadata/annotations.yaml
+yq eval -i '.annotations."com.redhat.openshift.versions" = "v4.9"' bundle/metadata/annotations.yaml
 yq eval -i '.annotations."com.redhat.delivery.operator.bundle" = true' bundle/metadata/annotations.yaml
 yq eval -i '.annotations."com.redhat.delivery.backport" = true' bundle/metadata/annotations.yaml
 yq eval -i '.annotations."com.redhat.openshift.versions" headComment = "Certified Openshift required labels"' bundle/metadata/annotations.yaml
@@ -23,5 +23,18 @@ createdAt=$(date +%Y-%m-%d) yq eval '.metadata.annotations.createdAt = env(creat
 # Use the correct containerImage from the deployment
 yq eval '.metadata.annotations.containerImage = .spec.install.spec.deployments[0].spec.template.spec.containers[0].image' -i bundle/manifests/cass-operator.clusterserviceversion.yaml
 
+# base64 command differs in every OS it seems..
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    BASE64_DS_IMG=$(base64 -w 80 -i config/manifests/images/ds.png)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    BASE64_DS_IMG=$(base64 -b 80 -i config/manifests/images/ds.png)
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    # FreeBSD seems to by default set 72 chars, which is fine
+    BASE64_DS_IMG=$(base64 -e config/manifests/images/ds.png)
+else
+    # No idea what system this could be.. lets assume gnu tools
+    BASE64_DS_IMG=$(base64 -w 80 -i config/manifests/images/ds.png)
+fi
+
 # Add ds.png as base64 encoded icondata
-yq eval '.spec.icon[0].base64data = "'"$(base64 -w 80 -i config/manifests/images/ds.png)"'"' -i bundle/manifests/cass-operator.clusterserviceversion.yaml
+yq eval '.spec.icon[0].base64data = "'"$BASE64_DS_IMG"'"' -i bundle/manifests/cass-operator.clusterserviceversion.yaml

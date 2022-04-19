e105c6e82263746d04dd8c7fbdf5066bd682478d
Michael Burman <michael.burman@datastax.com>
2022-04-19 15:15:11 +0300

    Revert logging changes

--- tests/decommission_dc/decommission_dc_suite_test.go
+++ tests/decommission_dc/decommission_dc_suite_test.go
@@ -65,8 +65,6 @@ func findDatacenters(nodeName string) []string {
 		dcs = append(dcs, strings.TrimSpace(dcParts[1]))
 	}
 
-	fmt.Printf("Nodetool status output:\n\n%s\n", output)
-
 	return dcs
 }
 

fd79c991396ec80546786e28a8a8697e21cd886d
Michael Burman <yak@iki.fi>
2022-07-19 17:58:03 +0300

    Post-release v1.12.0

--- scripts/post-release-process.sh
+++ scripts/post-release-process.sh
@@ -3,7 +3,10 @@ IMG=k8ssandra/cass-operator:latest
 KUSTOMIZE=$(pwd)/bin/kustomize
 
 # Add new ## unreleased after the tagging (post-release-process.sh)
+gawk -i inplace  '/##/ && ++c==1 { print "## unreleased\n"; print; next }1' CHANGELOG.md
+
 # Modify Makefile for the next VERSION in line
+
 
 # Return config/manager/kustomization.yaml to :latest
 cd config/manager && $KUSTOMIZE edit set image controller=$IMG && cd -

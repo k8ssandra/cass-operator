31da6417ae26dc46580dbbe365bdf47a0d726cac
Michael Burman <michael.burman@datastax.com>
2023-08-28 14:43:48 +0300

    Set M_SERVER_TYPE  and M_SERVER_VERSION for kind-311 tests, add more logging

--- tests/test_all_the_things/test_all_the_things_suite_test.go
+++ tests/test_all_the_things/test_all_the_things_suite_test.go
@@ -143,6 +143,14 @@ var _ = Describe(testName, func() {
 			ns.ExecAndLog(step, k)
 			wg.Wait()
 
+			found, err := regexp.MatchString("node/drain status=200 OK", logOutput)
+			if err == nil && !found {
+				ns.Log(fmt.Sprintf("logOutput, pod: statefulset.kubernetes.io/pod-name=cluster1-%s-r1-sts-0 => %s", api.CleanupForKubernetes(dcNameOverride), logOutput))
+			}
+			if err != nil {
+				ns.Log(fmt.Sprintf("Regexp parsing failed: %v", err))
+			}
+
 			// Check the log contains node/drain..
 			Expect(regexp.MatchString("node/drain status=200 OK", logOutput)).To(BeTrue())
 

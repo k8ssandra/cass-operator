00d7c997da80ccc905d30ccfe0687f801d06e7b9
Michael Burman <yak@iki.fi>
2024-11-06 17:05:23 -0800

    A scheduler plugin imitator that tries to verify if proposed pods will be schedulable in the cluster

--- go.mod
+++ go.mod
@@ -26,11 +26,13 @@ require (
 	github.com/prometheus/client_golang v1.18.0
 	go.uber.org/zap v1.26.0
 	golang.org/x/mod v0.17.0
+	k8s.io/kubernetes v1.29.2
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
@@ -74,9 +76,12 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.29.2 // indirect
+	k8s.io/apiserver v0.29.2 // indirect
 	k8s.io/component-base v0.29.2 // indirect
+	k8s.io/component-helpers v0.29.2 // indirect
 	k8s.io/klog/v2 v2.110.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
+	k8s.io/kube-scheduler v0.29.2 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
---
+++ pkg/scheduler/fit.go
@@ -0,0 +1,108 @@
+package scheduler
+
+import (
+	"context"
+	"errors"
+
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+	plfeature "k8s.io/kubernetes/pkg/scheduler/framework/plugins/feature"
+	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/interpodaffinity"
+	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/nodeaffinity"
+	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/noderesources"
+	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/nodeunschedulable"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func createNodeInfos(ctx context.Context, cli client.Client) ([]*framework.NodeInfo, error) {
+	nodes := corev1.NodeList{}
+	if err := cli.List(ctx, &nodes); err != nil {
+		return nil, err
+	}
+
+	nodeInfos := make([]*framework.NodeInfo, 0, len(nodes.Items))
+
+	for _, node := range nodes.Items {
+		nodeInfo := framework.NewNodeInfo()
+		nodeInfo.SetNode(&node)
+		pods := &corev1.PodList{}
+		if err := cli.List(ctx, pods, client.MatchingFields{"spec.nodeName": nodeInfo.Node().Name}); err != nil {
+			return nil, err
+		}
+
+		for _, pod := range pods.Items {
+			nodeInfo.AddPod(&pod)
+		}
+
+		nodeInfos = append(nodeInfos, nodeInfo)
+	}
+
+	return nodeInfos, nil
+}
+
+// WillTheyFit checks if the proposed pods will be schedulable in the cluster. The ProposedPods must have their
+// labels, tolerations, affinities and resource limits set
+func WillTheyFit(ctx context.Context, cli client.Client, proposedPods []*corev1.Pod) error {
+	usableNodes, err := createNodeInfos(ctx, cli)
+	if err != nil {
+		return err
+	}
+
+	state := framework.NewCycleState()
+
+	schedulablePlugin, err := nodeunschedulable.New(ctx, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	noderesourcesPlugin, err := noderesources.NewFit(ctx, nil, nil, plfeature.Features{})
+	if err != nil {
+		return err
+	}
+
+	nodeaffinityPlugin, err := nodeaffinity.New(ctx, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	interpodaffinityPlugin, err := interpodaffinity.New(ctx, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	plugins := []framework.FilterPlugin{
+		schedulablePlugin.(framework.FilterPlugin),
+		noderesourcesPlugin.(framework.FilterPlugin),
+		nodeaffinityPlugin.(framework.FilterPlugin),
+		interpodaffinityPlugin.(framework.FilterPlugin),
+	}
+
+NextPod:
+	for _, pod := range proposedPods {
+		for _, node := range usableNodes {
+			podInfo, err := framework.NewPodInfo(pod)
+			if err != nil {
+				return err
+			}
+
+			for _, plugin := range plugins {
+				status := plugin.Filter(ctx, state, podInfo.Pod, node)
+				if status.Code() == framework.Unschedulable {
+					continue
+				}
+			}
+
+			node.AddPod(pod)
+			continue NextPod
+			// Now we need a way to check if the pod was never added to any node
+			// TODO Need more checks like affinities, inter-pod affinities, taints, tolerations
+		}
+		// Pod was never added to any node
+		return errors.New(framework.Unschedulable.String())
+	}
+
+	// Check node condition, will it accept something
+	// Need tolerations, taints, affinities..
+	return nil
+}

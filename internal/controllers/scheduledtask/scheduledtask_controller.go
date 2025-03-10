55fef66f9faaf4e432376122107be1a417e7b79e
Miles Garnsey <miles.garnsey@datastax.com>
2025-03-10 13:20:00 +1100

    Fix roles.

--- internal/controllers/scheduledtask/scheduledtask_controller.go
+++ internal/controllers/scheduledtask/scheduledtask_controller.go
@@ -53,6 +53,10 @@ func (r *RealClock) Now() time.Time {
 	return time.Now()
 }
 
+//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,resources=scheduledtask,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,resources=scheduledtask/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,resources=scheduledtask/finalizers,verbs=update
+
 func (r *ScheduledTaskReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	scheduledtask := &scheduledtaskv1alpha1.ScheduledTask{}
 	err := r.Get(ctx, req.NamespacedName, scheduledtask)

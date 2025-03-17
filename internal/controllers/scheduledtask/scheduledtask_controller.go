dc2db88a09fecef7e90fadae844004dd19463bfd
Miles Garnsey <miles.garnsey@datastax.com>
2025-03-17 21:11:56 +1100

    Fix perms so they land in role for non-cluster scoped deployments.

--- internal/controllers/scheduledtask/scheduledtask_controller.go
+++ internal/controllers/scheduledtask/scheduledtask_controller.go
@@ -53,9 +53,9 @@ func (r *RealClock) Now() time.Time {
 	return time.Now()
 }
 
-//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,resources=scheduledtask,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,resources=scheduledtask/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,resources=scheduledtask/finalizers,verbs=update
+//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,namespace=cass-operator,resources=scheduledtask,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,namespace=cass-operator,resources=scheduledtask/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=scheduledtask.k8ssandra.io,namespace=cass-operator,resources=scheduledtask/finalizers,verbs=update
 
 func (r *ScheduledTaskReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	scheduledtask := &scheduledtaskv1alpha1.ScheduledTask{}

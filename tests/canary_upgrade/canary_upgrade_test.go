c25a6841a9db56b5015c940488f757a6d9a8b69b
Michael Burman <yak@iki.fi>
2025-03-21 11:16:44 +0200

    Fix testFile generation for canary, node_move and upgrade_operator.

--- tests/canary_upgrade/canary_upgrade_test.go
+++ tests/canary_upgrade/canary_upgrade_test.go
@@ -59,7 +59,10 @@ var _ = Describe(testName, func() {
 			ns.WaitForOperatorReady()
 
 			step := "creating a datacenter"
-			k := kubectl.ApplyFiles(dcYaml)
+			testFile, err := ginkgo_util.CreateTestFile(dcYaml)
+			Expect(err).ToNot(HaveOccurred())
+
+			k := kubectl.ApplyFiles(testFile)
 			ns.ExecAndLog(step, k)
 
 			ns.WaitForSuperUserUpserted(dcName, 600)

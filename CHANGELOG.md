7a72b2353e5528433f872c6656919a19924475c6
Michael Burman <yak@iki.fi>
2025-09-08 13:59:07 +0300

    Prepare for next version 1.28.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ Changelog for Cass Operator, new PRs should update the `main / unreleased` secti
 * [BUGFIX]
 ```
 
+## unreleased
+
 ## v1.27.0
 
 * [CHANGE] [#822](https://github.com/k8ssandra/cass-operator/issues/822) Remove certain RBAC rights from our Role/ClusterRole that are no longer necessary for the operations in cass-operator
--- scripts/post-release-process.sh
+++ scripts/post-release-process.sh
@@ -42,11 +42,12 @@ yq eval -i '.images.config-builder |= sub("cr.dtsx.io/", "")' config/manager/ima
 yq eval -i '.defaults.dse.repository |= sub("cr.dtsx.io/", "")' config/manager/image_config.yaml
 
 # Update new imageconfig
-yq eval -i '.images.k8ssandra-client.tag = "latest"' config/imageconfig/image_config.yaml
+yq eval -i '.images.system-logger.tag = "latest"' config/imageconfig/image_config.yaml
 yq eval -i "del(.images.config-builder.registry)" config/imageconfig/image_config.yaml
 yq eval -i "del(.types.dse.registry)" config/imageconfig/image_config.yaml
 yq eval -i "del(.images.k8ssandra-client.registry)" config/imageconfig/image_config.yaml
 yq eval -i '.types.cassandra.registry = "ghcr.io"' config/imageconfig/image_config.yaml
+yq eval -i '.images.system-logger.registry = "ghcr.io"' config/imageconfig/image_config.yaml
 
 # Commit to git
 NEXT_VERSION=$(gawk 'match($0, /^VERSION \?= /) { print substr($0, RLENGTH+1)}' Makefile)

5e022cab18a85db27d2d4ec0a03ec589c378ac5e
Michael Burman <yak@iki.fi>
2026-09-22 13:09:45 +0300

    Add debug logging

--- hack/cluster.sh
+++ hack/cluster.sh
@@ -67,7 +67,7 @@ fi
 # https://github.com/kubernetes-sigs/kind/issues/2875
 # https://github.com/containerd/containerd/blob/main/docs/cri/config.md#registry-configuration
 # See: https://github.com/containerd/containerd/blob/main/docs/hosts.md
-cat <<EOF | kind create cluster --config=-
+cat <<EOF | kind create cluster --retain --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 containerdConfigPatches:
